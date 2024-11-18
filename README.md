ed048755ea913c35160c6b2dc6cb1b23c93fdc63
giangndct <truonggiangg0802@gmail.com>
2024-11-18 22:31:34 +0700

    update code and readme

--- README.md
+++ README.md
@@ -1,66 +1,26 @@
 # Hướng dẫn Cài đặt
 
-## Bước 1: Cài đặt thư viện
+- Sử dụng python 3.10.15
 
-- cài theo fiel requirements.txt
-- cài xong thì mở cmd chạy 'svcg' nếu hiện ra giao diện ![alt text](assets/images/image.png) là thành công
+## Bước 1: Tạo môi trường ảo
+```bash
+python3.11 -m venv venv
+```
+- Kích hoạt môi trường ảo
+```bash
+source venv/bin/activate
+```
+## Bước 2: Cài đặt thư viện
 
-## Bước 2: Tải dataset
+- cài thư viện pytorch và so-vits-svc
 
-- vào link : https://drive.google.com/drive/folders/1rb2syP4U7jYz_z9WoG7MVR6t3i375smZ?usp=sharing
-- tải folder dataset về đổi tên thành dataset_raw
+```bash
+pip install -U torch torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install -U so-vits-svc-fork
+```
 
-## Bước 3
+- cài theo file requirements.txt
 
-- Tìm kiếm folder thư viện so-vits-svc-fork mà mình cài ở máy ảo
-- cho folder dataset_raw vào trong đó như hình ![alt text](assets/images/image-1.png)
-- mở cmd tại đó luôn
-- chạy lần lượt 4 lệnh
-  - svc pre-resample
-  - svc pre-config
-  - svc pre-hubert
-  - svc train -t
-
-# Hướng dẫn cắt audio
-
-- ![alt text](assets/images/image-4.png) sửa 2 cái này. audio cần cắt cứ nhét hết vào file dataset_raw_raw.
-- Folder output thì trong dataset_raw hãy tạo 1 folder trên ca sĩ rồi cho output đã split vô đó
-
-
-# Endpoints api
-- GET /models?category=?
-  - Input: 
-  - OutPut:
-    - id_model: int
-    - model_name: str
-    - model_path: str
-    - config_path: str
-    - cluster_model_path: str
-    - category :str
-- POST /text-to-speech/
-  - Input: text: str
-  - OutPut: output.wav 
-- POST /text-file-to-speech/
-  - Input: file .docx/.txt
-  - OutPut: output.wav
-- POST /text-file-to-speech-and-infer/
-  - Input : 
-    - file .docx/.txt
-    - model_id: int
-  - Output: output.wav
-- POST /text-to-speech-and-infer/
-  - Input: 
-    - text: str
-    - model_id: int
-  - Output: output.wav
-- POST /infer-audio/
-  - Input: 
-    - file audio.wav
-    - model_id: int
-  - Ouput: output.wav
-- POST /train-model/
-  - Input: 
-    - file audio.wav
-    - name: str
-    - f0_method: ["crepe", "crepe-tiny", "parselmouth", "dio", "harvest"] (chọn 1)
-  - OutPut: Tạm thời là "message": "Audio processing completed successfully!"
+```bash
+pip install -r requirements.txt
+```
--- code.py
+++ code.py
@@ -1,4 +1,13 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException, Form, Response, Depends, Query
+from fastapi import (
+    FastAPI,
+    UploadFile,
+    File,
+    HTTPException,
+    Form,
+    Response,
+    Depends,
+    Query,
+)
 from fastapi.responses import FileResponse, JSONResponse
 from pathlib import Path
 import tempfile
@@ -27,14 +36,19 @@ from so_vits_svc_fork.preprocessing.preprocess_flist_config import (
 from so_vits_svc_fork.preprocessing.preprocess_hubert_f0 import preprocess_hubert_f0
 from so_vits_svc_fork.train import train
 import shutil
+import shutil
+import asyncio
+import aiofiles
+import uvicorn
 
 # Kết nối tới cơ sở dữ liệu MySQL
 DATABASE_URL = "mysql+pymysql://root:g08022002@localhost/datn"  # Thay đổi username, password, và dbname của bạn
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-section_storage_path = "D:/files/"
-os.makedirs(section_storage_path, exist_ok=True)
+BASE_DIR = Path(__file__).resolve().parent
+section_storage_path = BASE_DIR / "files"
+train_model_path = BASE_DIR / "trainmodel"
 
 
 # Định nghĩa cơ sở dữ liệu
@@ -52,7 +66,7 @@ class Model(Base):
     model_path = Column(String(200))
     config_path = Column(String(200))
     cluster_model_path = Column(String(200), nullable=True)
-    category=Column(String(200), nullable=True)
+    category = Column(String(200), nullable=True)
 
 
 # Tạo FastAPI app
@@ -77,7 +91,7 @@ class ModelResponse(BaseModel):
     model_path: str
     config_path: str
     cluster_model_path: str
-    category :str
+    category: str
 
     class Config:
         from_attributes = True
@@ -102,6 +116,22 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+async def save_uploaded_file(uploaded_file, destination: Path):
+    try:
+        with destination.open("wb") as dest_file:
+            while True:
+                chunk = await asyncio.to_thread(
+                    uploaded_file.read, 1024
+                )  # Đọc từng khối 1024 byte
+                if not chunk:
+                    break
+                dest_file.write(chunk)
+    except Exception as e:
+        raise RuntimeError(f"Error saving uploaded file: {e}")
+
+
 def pre_split(
     input_dir: str,
     output_dir: str,
@@ -228,7 +258,7 @@ def train(
 @app.get("/models", response_model=List[ModelResponse])
 async def get_models(
     db: Session = Depends(get_db),
-    category: Optional[str] = Query(None, title="Category to filter")
+    category: Optional[str] = Query(None, title="Category to filter"),
 ):
     if category:
         # Lọc mô hình theo category
@@ -236,7 +266,7 @@ async def get_models(
     else:
         # Lấy tất cả mô hình nếu không có filter
         models = db.query(Model).all()
-    
+
     return models
 
 
@@ -269,7 +299,7 @@ async def update_model(
     model_path: str = Form(...),
     config_path: str = Form(...),
     cluster_model_path: str = Form(None),
-    category: str = Form(...),  
+    category: str = Form(...),
     db: Session = Depends(get_db),
 ):
     model_info = db.query(Model).filter(Model.id_model == model_id).first()
@@ -307,7 +337,8 @@ async def text_to_speech(request: TextToSpeechRequest):
     # Generate audio file
     try:
         tts = gTTS(text=request.text, lang=request.locate)
-        tts.save(audio_file_path)
+        # Lưu file âm thanh trong một luồng riêng biệt
+        await asyncio.to_thread(tts.save, section_cloned_file_path)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error generating audio: {str(e)}")
 
@@ -329,8 +360,8 @@ async def text_file_to_speech(
         # Chuyển đổi văn bản thành giọng nói
         try:
             tts = gTTS(text=text, lang=locate)
-            tts.save(audio_file_path)
-            # print(f"File âm thanh đã được lưu tại: {audio_file_path}")
+            # Lưu file âm thanh trong một luồng riêng biệt
+            await asyncio.to_thread(tts.save, audio_file_path)
         except Exception as e:
             raise HTTPException(
                 status_code=500, detail=f"Error generating audio: {str(e)}"
@@ -348,8 +379,8 @@ async def text_file_to_speech(
         audio_file_path = os.path.join(section_storage_path, section_id + ".wav")
         try:
             tts = gTTS(text=text, lang=locate)
-            tts.save(audio_file_path)
-            # print(f"File âm thanh đã được lưu tại: {audio_file_path}")
+            # Lưu file âm thanh trong một luồng riêng biệt
+            await asyncio.to_thread(tts.save, audio_file_path)
         except Exception as e:
             raise HTTPException(
                 status_code=500, detail=f"Error generating audio: {str(e)}"
@@ -372,19 +403,20 @@ async def text_file_to_speech_and_infer(
 ):
     if file.filename.endswith(".txt"):
         # Đọc nội dung từ tệp văn bản
+        os.makedirs(section_storage_path, exist_ok=True)
+        os.makedirs(train_model_path, exist_ok=True)
+
         content = await file.read()
         text = content.decode("utf-8")  # Giải mã nội dung
 
         # Lấy model_path và config_path từ cơ sở dữ liệu
-        try:
-            model = db.execute(
-                select(Model).where(Model.id_model == model_id)
-            ).scalar_one()
-            model_path = model.model_path
-            config_path = model.config_path
-        except NoResultFound:
-            raise HTTPException(status_code=404, detail="Model not found")
+        model_info = db.query(Model).filter(Model.id_model == model_id).first()
+        if not model_info:
+            raise HTTPException(status_code=400, detail="Model not found")
 
+        model_path = BASE_DIR / model_info.model_path
+        config_path = BASE_DIR / model_info.config_path
+        cluster_model_path = BASE_DIR / model_info.cluster_model_path
         # Tạo tên file âm thanh ngẫu nhiên
         section_id = str(uuid.uuid4())
         audio_file_path = os.path.join(section_storage_path, section_id + ".wav")
@@ -392,8 +424,8 @@ async def text_file_to_speech_and_infer(
         # Chuyển đổi văn bản thành giọng nói
         try:
             tts = gTTS(text=text, lang=locate)
-            tts.save(audio_file_path)
-            # print(f"File âm thanh đã được lưu tại: {audio_file_path}")
+            # Lưu file âm thanh trong một luồng riêng biệt
+            await asyncio.to_thread(tts.save, audio_file_path)
         except Exception as e:
             raise HTTPException(
                 status_code=500, detail=f"Error generating audio: {str(e)}"
@@ -405,13 +437,18 @@ async def text_file_to_speech_and_infer(
 
         # Gọi hàm infer với file âm thanh vừa tạo ra
         try:
-            infer(
-                input_path=audio_file_path,  # Sử dụng tệp âm thanh đã lưu
+            await asyncio.to_thread(
+                infer,
+                input_path=audio_file_path,
                 output_path=Path(output_audio_path),
                 model_path=Path(model_path),
                 config_path=Path(config_path),
-                speaker=0,  # Thay đổi ID của speaker nếu cần
-                cluster_model_path=None,
+                speaker=0,
+                cluster_model_path=(
+                    None
+                    if model_info.cluster_model_path is None
+                    else Path(cluster_model_path)
+                ),
                 transpose=0,
                 auto_predict_f0=True,
                 cluster_infer_ratio=0.0,
@@ -433,18 +470,20 @@ async def text_file_to_speech_and_infer(
             output_audio_path, media_type="audio/wav", filename="output.wav"
         )
     elif file.filename.endswith(".docx"):
+        os.makedirs(section_storage_path, exist_ok=True)
+        os.makedirs(train_model_path, exist_ok=True)
         # Đọc nội dung từ tệp Word
         content = await file.read()
         doc = Document(io.BytesIO(content))
         text = "\n".join([para.text for para in doc.paragraphs])
-        try:
-            model = db.execute(
-                select(Model).where(Model.id_model == model_id)
-            ).scalar_one()
-            model_path = model.model_path
-            config_path = model.config_path
-        except NoResultFound:
-            raise HTTPException(status_code=404, detail="Model not found")
+        # Lấy model_path và config_path từ cơ sở dữ liệu
+        model_info = db.query(Model).filter(Model.id_model == model_id).first()
+        if not model_info:
+            raise HTTPException(status_code=400, detail="Model not found")
+
+        model_path = BASE_DIR / model_info.model_path
+        config_path = BASE_DIR / model_info.config_path
+        cluster_model_path = BASE_DIR / model_info.cluster_model_path
 
         # Tạo tên file âm thanh ngẫu nhiên
         section_id = str(uuid.uuid4())
@@ -453,8 +492,8 @@ async def text_file_to_speech_and_infer(
         # Chuyển đổi văn bản thành giọng nói
         try:
             tts = gTTS(text=text, lang=locate)
-            tts.save(audio_file_path)
-            # print(f"File âm thanh đã được lưu tại: {audio_file_path}")
+            # Lưu file âm thanh trong một luồng riêng biệt
+            await asyncio.to_thread(tts.save, audio_file_path)
         except Exception as e:
             raise HTTPException(
                 status_code=500, detail=f"Error generating audio: {str(e)}"
@@ -466,13 +505,14 @@ async def text_file_to_speech_and_infer(
 
         # Gọi hàm infer với file âm thanh vừa tạo ra
         try:
-            infer(
-                input_path=audio_file_path,  # Sử dụng tệp âm thanh đã lưu
+            await asyncio.to_thread(
+                infer,
+                input_path=audio_file_path,
                 output_path=Path(output_audio_path),
                 model_path=Path(model_path),
                 config_path=Path(config_path),
-                speaker=0,  # Thay đổi ID của speaker nếu cần
-                cluster_model_path=None,
+                speaker=0,
+                cluster_model_path=Path(cluster_model_path),
                 transpose=0,
                 auto_predict_f0=True,
                 cluster_infer_ratio=0.0,
@@ -497,22 +537,20 @@ async def text_file_to_speech_and_infer(
         raise HTTPException(status_code=400, detail="Invalid file format.")
 
 
-# API TTS và infer
 @app.post("/text-to-speech-and-infer/")
-async def text_to_speech_and_process(request: TextToSpeechAndInferRequest):
-    # Tạo phiên làm việc với cơ sở dữ liệu
-    db: Session = SessionLocal()
-
-    # Lấy model_path và config_path từ cơ sở dữ liệu
-    try:
-        model = db.execute(
-            select(Model).where(Model.id_model == request.model_id)
-        ).scalar_one()
-        model_path = model.model_path
-        config_path = model.config_path
-    except NoResultFound:
-        raise HTTPException(status_code=404, detail="Model not found")
-
+async def text_to_speech_and_process(
+    request: TextToSpeechAndInferRequest, db: Session = Depends(get_db)
+):
+    # Kiểm tra thông tin mô hình trong cơ sở dữ liệu
+    model_info = db.query(Model).filter(Model.id_model == request.model_id).first()
+    if not model_info:
+        raise HTTPException(status_code=400, detail="Model not found")
+    os.makedirs(section_storage_path, exist_ok=True)
+    os.makedirs(train_model_path, exist_ok=True)
+    # Lấy thông tin đường dẫn mô hình và cấu hình từ cơ sở dữ liệu
+    model_path = BASE_DIR / model_info.model_path
+    config_path = BASE_DIR / model_info.config_path
+    cluster_model_path = BASE_DIR / model_info.cluster_model_path
     # Tạo tên file ngẫu nhiên
     section_id = str(uuid.uuid4())
     section_cloned_file_path = os.path.join(section_storage_path, section_id + ".wav")
@@ -520,23 +558,24 @@ async def text_to_speech_and_process(request: TextToSpeechAndInferRequest):
     # Sử dụng gTTS để chuyển đổi văn bản thành giọng nói
     try:
         tts = gTTS(text=request.text, lang=request.locate)
-        tts.save(section_cloned_file_path)
-        # print(f"File âm thanh đã được lưu tại: {section_cloned_file_path}")
+        # Lưu file âm thanh trong một luồng riêng biệt
+        await asyncio.to_thread(tts.save, section_cloned_file_path)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error generating audio: {str(e)}")
 
-    # Gọi hàm infer với file âm thanh vừa tạo ra
+    # Gọi hàm infer để xử lý âm thanh (sử dụng asyncio.to_thread nếu infer là đồng bộ)
     output_audio_path = os.path.join(
         section_storage_path, f"{section_id}_processed.wav"
     )
     try:
-        infer(
+        await asyncio.to_thread(
+            infer,
             input_path=section_cloned_file_path,
             output_path=Path(output_audio_path),
             model_path=Path(model_path),
             config_path=Path(config_path),
             speaker=0,
-            cluster_model_path=None,
+            cluster_model_path=Path(cluster_model_path),
             transpose=0,
             auto_predict_f0=True,
             cluster_infer_ratio=0.0,
@@ -548,7 +587,6 @@ async def text_to_speech_and_process(request: TextToSpeechAndInferRequest):
             absolute_thresh=False,
             max_chunk_seconds=40,
         )
-        # print(f"Audio đã được xử lý và lưu tại: {output_audio_path}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing audio: {str(e)}")
 
@@ -557,36 +595,40 @@ async def text_to_speech_and_process(request: TextToSpeechAndInferRequest):
     )
 
 
-# API infer audio
 @app.post("/infer-audio/", response_class=FileResponse)
 async def upload_and_infer(
     file: UploadFile = File(...),
     model_id: int = Form(...),
     db: Session = Depends(get_db),
 ):
+
     # Kiểm tra thông tin mô hình trong cơ sở dữ liệu
     model_info = db.query(Model).filter(Model.id_model == model_id).first()
     if not model_info:
         raise HTTPException(status_code=400, detail="Model not found")
-
+    os.makedirs(section_storage_path, exist_ok=True)
+    os.makedirs(train_model_path, exist_ok=True)
     input_id = str(uuid.uuid4())
     output_id = str(uuid.uuid4())
 
     input_file_path = os.path.join(section_storage_path, f"{input_id}.wav")
     output_file_path = os.path.join(section_storage_path, f"{output_id}_processed.wav")
 
-    with open(input_file_path, "wb") as buffer:
+    cluster_model_path = BASE_DIR / model_info.cluster_model_path
+    # Lưu tệp âm thanh được tải lên bằng aiofiles
+    async with aiofiles.open(input_file_path, "wb") as buffer:
         content = await file.read()
-        buffer.write(content)
+        await buffer.write(content)
 
-    # Gọi hàm infer để xử lý âm thanh
-    infer(
+    # Gọi hàm infer để xử lý âm thanh (cần đảm bảo infer là hàm async hoặc sử dụng asyncio.to_thread nếu không đồng bộ)
+    await asyncio.to_thread(
+        infer,
         input_path=input_file_path,  # Sử dụng tệp đã lưu
         output_path=Path(output_file_path),
-        model_path=Path(model_info.model_path),
-        config_path=Path(model_info.config_path),
+        model_path=Path(BASE_DIR / model_info.model_path),
+        config_path=Path(BASE_DIR / model_info.config_path),
         speaker=0,
-        cluster_model_path=Path(model_info.cluster_model_path),
+        cluster_model_path=Path(cluster_model_path),
         transpose=0,
         auto_predict_f0=True,
         cluster_infer_ratio=0.0,
@@ -598,20 +640,24 @@ async def upload_and_infer(
         absolute_thresh=False,
         max_chunk_seconds=40,
     )
+
     print(f"Audio đã được xử lý và lưu tại: {output_file_path}")
     return FileResponse(output_file_path, media_type="audio/wav", filename="output.wav")
+
 
 @app.post("/train-model/")
 async def process_audio(
     name: str = Form(...), file: UploadFile = File(...), f0_method: str = Form(...)
 ):
     # Tạo thư mục tạm để lưu file âm thanh
-    input_dir = "D:/DATN/Code/audio_data"
-    output_dir = f"D:/DATN/Code/trainmodel/{name}"
-    input_train = f"D:/DATN/Code/trainmodel/{name}/dataset/44k"
-    output_split = f"D:/DATN/Code/trainmodel/{name}/dataset_raw"
+    input_dir = BASE_DIR / "audio_data"
+    output_dir = BASE_DIR / f"trainmodel/{name}"
+    input_train = BASE_DIR / f"trainmodel/{name}/dataset/44k"
+    output_split = BASE_DIR / f"trainmodel/{name}/dataset_raw"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+    if not os.path.exists(input_dir):
+        os.makedirs(input_dir, exist_ok=True)
 
     temp_file_path = Path(input_dir) / file.filename
 
@@ -619,20 +665,20 @@ async def process_audio(
     if not os.path.exists(input_dir):
         os.makedirs(input_dir)
 
-    with open(temp_file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+    await save_uploaded_file(file.file, temp_file_path)
 
     # Chạy lần lượt các hàm xử lý
     try:
         # Bước 1: Pre-split
-        pre_split(
+        await asyncio.to_thread(
+            pre_split,
             input_dir=input_dir,
             output_dir=os.path.join(output_dir, f"dataset_raw/{name}"),
             sr=22050,
         )
-
         # Bước 2: Process Audio Files
-        process_audio_files(
+        await asyncio.to_thread(
+            process_audio_files,
             input_dir=output_split,
             output_dir=input_train,
             sampling_rate=16000,
@@ -642,16 +688,16 @@ async def process_audio(
         config_dir = os.path.join(output_dir, "configs/44k/config.json")
         filelist_dir = os.path.join(output_dir, "filelists/44k")
         config_type = "so-vits-svc-4.0v1"
-        pre_config(
+        await asyncio.to_thread(
+            pre_config,
             input_dir=input_train,
             filelist_path=filelist_dir,
             config_path=config_dir,
             config_type=config_type,
         )
-
         # Bước 4: Pre-hubert
-
-        pre_hubert(
+        await asyncio.to_thread(
+            pre_hubert,
             input_dir=input_train,
             config_path=config_dir,
             n_jobs=None,
@@ -659,9 +705,10 @@ async def process_audio(
             f0_method=f0_method,
         )
 
-        # # Bước 5: Train model
-        # model_dir = os.path.join(output_dir, "dataset/44k")  # Cần thay bằng đường dẫn chính xác tới thư mục model
-        # train(
+        # Bước 5: Train model
+        # model_dir = os.path.join(output_dir, "logs/44k")
+        # await asyncio.to_thread(
+        #     train,
         #     config_path=config_dir,
         #     model_path=model_dir,
         #     tensorboard=False,
@@ -672,6 +719,7 @@ async def process_audio(
 
     except Exception as e:
         return {"error": str(e)}
+
 
 if __name__ == "__main__":
     import uvicorn
